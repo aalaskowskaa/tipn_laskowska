ccb9d8c49598ba2694fd5d2310db0f5460b4dcb0
aalaskowskaa <aleksandra.laskowska@gumed.edu.pl>
2018-03-08 18:35:46 +0100

    zad2-3

---
+++ zad2-3.R
@@ -0,0 +1,11 @@
+lata_przestepne <- c(1800:2018)
+
+for(i in lata_przestepne) {
+  if(i %% 4 == 0) {
+    if(i %% 100 != 0) {
+      print(i)
+    }
+  } else if(i %% 400 == 0) {
+    print(i)
+  }
+}
